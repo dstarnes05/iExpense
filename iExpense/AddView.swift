bb9d92fab558e3b0b3045911c2edb1fffc4f652b
Daniel Starnes <dstarnes261@gmail.com>
2026-02-06 21:54:39 -0600

    changed name field in AddView to appear as navigation title; *may change back*

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+    @State private var name = "Expense Name"
     @State private var type = "Personal"
     @State private var amount = 0.0
     
@@ -22,7 +22,7 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
+                //TextField("Name", text: $name)
                 
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
@@ -33,7 +33,8 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add new expense")
+            .navigationTitle($name)
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
                     Button("Save") {
